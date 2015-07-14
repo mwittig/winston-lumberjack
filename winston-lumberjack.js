bc3a53e6c03aee1f611b80fea6e097ccdeb6438a
marcus <WittigMarcus@gmail.com>
2015-07-14 08:20:51 +0200

    Transport implementation

--- winston-lumberjack.js
+++ winston-lumberjack.js
@@ -11,12 +11,24 @@ var net = require('net'),
     winston = require('winston'),
     common = require('winston/lib/winston/common'),
     _ = require('lodash'),
+    stringify = require('json-stringify-safe'),
+    lumberjackProtocol = require('lumberjack-protocol'),
     Lumberjack = exports.Lumberjack = function (options) {
         winston.Transport.call(this, options);
         options = options || {};
 
         this.name                = 'lumberjack';
+        this.localhost           = options.localhost || os.hostname();
+        this.node_name           = options.node_name || process.title;
+        this.pid                 = options.pid || process.pid;
+        this.serverType          = options.serverType || "Unknown";
+        this.label               = options.label || 'Unknown';
+        this.timestamp           = options.timestamp || false;
 
+        this.tlsOptions          = options.tlsOptions || {};
+        this.clientOptions       = options.clientOptions || {};
+
+        this.client = lumberjackProtocol.client(this.tlsOptions, this.clientOptions);
     };
 
 //
@@ -30,6 +42,64 @@ util.inherits(Lumberjack, winston.Transport);
 //
 winston.transports.Lumberjack = Lumberjack;
 
-Lumberjack.prototype.log = function (level, msg, meta, callback) {
+function extractJSON(str) {
+    var firstOpen, lastClose, candidate;
 
+    firstOpen = str.indexOf('{');
+    do {
+        lastClose = str.lastIndexOf('}');
+        if (lastClose <= firstOpen) {
+            return null;
+        }
+        do {
+            candidate = str.substring(firstOpen, lastClose + 1);
+            try {
+                return res = JSON.parse(candidate);
+            }
+            catch(e) {}
+            lastClose = str.substr(0, lastClose).lastIndexOf('}');
+        } while(lastClose > firstOpen);
+        firstOpen = str.indexOf('{', firstOpen + 1);
+    } while(firstOpen != -1);
+    return null;
+}
+
+Lumberjack.prototype.log = function (level, msg, meta, callback) {
+    var self = this,
+        meta = {},
+        log_entry;
+
+    meta.details = winston.clone(_meta || {});
+    if (self.silent) {
+        return callback(null, true);
+    }
+
+    // add meta fields
+    meta.application = self.node_name;
+    meta.serverName = self.localhost;
+    meta.serverType = self.serverType;
+    meta.pid = self.pid;
+    meta.module = self.label;
+
+    // extract JSON object from message string (if any)
+    var json = extractJSON(msg);
+    if (json !== null) {
+        _.merge(meta.details, json);
+    }
+
+    // strip color codes from message part
+    msg = msg.replace(/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]/g, '');
+
+    log_entry = common.log({
+        level: level,
+        message: msg,
+        meta: meta,
+        // add timestamp if timestamp option is set to true
+        timestamp: this.timestamp,
+        json: true
+    });
+
+    self.client.writeDataFrame({
+        "line": stringify(data)
+    });
 };
